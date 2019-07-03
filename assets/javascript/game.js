fc6a8974b68acb33c4eb971340c4582f259bd393
Rhonda Kremer <rhondakremer@gmail.com>
2019-07-03 13:23:45 -0400

    created jquery click events that will designate hero and challenger

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,15 @@
 
 $(document).ready(function() {
 
+
+
+    var hero;
+    var challenger;
+    var heroSelected = false;
+    var challengerSelected = false;
+    selectHero();
+    selectChallenger();
+
     var characters = [
     rambo = { 
         name: "Rambo",
@@ -33,26 +42,119 @@ $(document).ready(function() {
         attackPoints: 10,
         counterAttackPower: 40,
     },
-    ]  
+    ]; 
+   
+    function selectHero() {
+    $("#rambopic" ).click(function() {
+        if (heroSelected === false) 
+        {
+        //alert("They drew first blood, not me.");
+        $("#rambopic").appendTo( $("#heropic"));
+        $("<p>This will show some info</p>").appendTo( $("#heroinfo"));
+        heroSelected = true;
+        hero = rambo;
+        console.log(hero);
+        }
+        });
     
-    $( "#rambopic" ).click(function() {
-        alert("They drew first blood, not me.");}
-      );
+
+    $("#judgedreddpic" ).click(function() {
+        if (heroSelected === false) 
+        {
+        //alert("The law never apologizes.");
+        $("#judgedreddpic").appendTo( $("#heropic"));
+        $("<p>This will show some info</p>").appendTo( $("#heroinfo"));
+        heroSelected = true;
+        hero = judgeDredd;
+        console.log(hero);
+        }
+        });
+
+    $("#rockypic" ).click(function() {
+        if (heroSelected === false) 
+        {
+        //alert("All I wanna do is go the distance.");
+        $("#rockypic").appendTo( $("#heropic"));
+        $("<p>This will show some info</p>").appendTo( $("#heroinfo"));
+        heroSelected = true;
+        hero = rocky;
+        console.log(hero);
+        }
+        });
+
+
+    $("#cobrapic" ).click(function() {
+        if (heroSelected === false) 
+        {
+        //alert("This is where the law ends and I start.");
+        $("#cobrapic").appendTo( $("#heropic"));
+        $("<p>This will show some info</p>").appendTo( $("#heroinfo"));
+        heroSelected = true;
+        hero = cobra;
+        console.log(hero);
+        }
+        });
+    }
+
+    function selectChallenger() {
+        $("#rambopic" ).click(function() {
+            if (challengerSelected === false && hero !== rambo) 
+            {
+            //alert("They drew first blood, not me.");
+            $("#rambopic").appendTo( $("#challengerpic"));
+            $("<p>This will show some more info</p>").appendTo( $("#challengerinfo"));
+            challengerSelected = true;
+            challenger = rambo;
+            console.log(challenger);
+            }
+            });
+        
     
-    $( "#judgedreddpic" ).click(function() {
-        alert("The law never apologizes.")
-        ;
-
-    );
-
+        $("#judgedreddpic" ).click(function() {
+            if (challengerSelected === false && hero !== judgeDredd)
+            {
+            //alert("The law never apologizes.");
+            $("#judgedreddpic").appendTo( $("#challengerpic"));
+            $("<p>This will show some more info</p>").appendTo( $("#challengerinfo"));
+            challengerSelected = true;
+            challenger = judgeDredd;
+            console.log(challenger);
+            }
+            });
+    
+        $("#rockypic" ).click(function() {
+            if (challengerSelected === false && hero !== rocky) 
+            {
+            //alert("All I wanna do is go the distance.");
+            $("#rockypic").appendTo( $("#challengerpic"));
+            $("<p>This will show some more info</p>").appendTo( $("#challengerinfo"));
+            challengerSelected = true;
+            challenger = rocky;
+            console.log(challenger);
+            }
+            });
+    
+    
+        $("#cobrapic" ).click(function() {
+            if (challengerSelected === false && hero !== cobra) 
+            {
+            //alert("This is where the law ends and I start.");
+            $("#cobrapic").appendTo( $("#challengerpic"));
+            $("<p>This will show some more info</p>").appendTo( $("#challengerinfo"));
+            challengerSelected = true;
+            challenger = cobra;
+            console.log(challenger);
+            }
+            });
+        }
 
 
        
+    //when 
+    //when health points=0, empty from div
 
+ 
 
-    //create a selection of characters (consider using objects)
-    //make each character selectable
-    //move first selected character to next section
     //make next selecter character the fighter
     //move figher to different area
     //button that says fight
